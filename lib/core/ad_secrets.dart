3b8768e6b56da49dd57a6caf6138f5002b499c64
ah11l <7oodygemar76@gmail.com>
2026-08-29 12:38:12 +0300

    Create ad_secrets.dart

---
+++ lib/core/ad_secrets.dart
@@ -0,0 +1,13 @@
+class AdSecrets {
+  static const String admobAppIdAndroid =
+      'ca-app-pub-2956752840406313~8414852612';
+
+  static const String admobBannerUnitId =
+      'ca-app-pub-2956752840406313/2710160443';
+
+  static const String admobInterstitialUnitId =
+      'ca-app-pub-2956752840406313/7490502382';
+
+  static const String admobRewardedUnitId =
+      'ca-app-pub-2956752840406313/7770915435';
+}
